6c136cd8f41271056b036fd353308cba905a26bd
unknown <you@example.com>
2022-09-07 23:02:49 -0600

    First

---
+++ src/calendar/components/Navbar.jsx
@@ -0,0 +1,14 @@
+export const Navbar = () => {
+  return (
+    <div className='navbar navbar-dark bg-dark mb-4 px-4'>
+      <span className='navbar-brand'>
+        <i className='fa-regular fa-calendar me-2'></i>
+        Alexander
+      </span>
+      <button className='btn btn-outline-danger'>
+        <i class='fa-solid fa-right-from-bracket me-2'></i>
+        <span>Salir</span>
+      </button>
+    </div>
+  );
+};
